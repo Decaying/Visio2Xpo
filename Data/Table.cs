b60abe6875df026bfd310897c48980cdc0158cf4
pvanstip <pvanstip@outlook.com>
2015-09-17 09:34:57 +0200

    Test
    In de class een extra variabele toegevoegd om de werking van github eens
    te checken

--- Data/Table.cs
+++ Data/Table.cs
@@ -9,5 +9,6 @@ namespace cvo.buyshans.Visio2Xpo.Data
         public String Name { get; set; }
         public IEnumerable<Field> Fields { get; set; }
         public PrimaryKey PrimaryKey { get; set; }
+        public String Description { get; set; }
     }
 }
